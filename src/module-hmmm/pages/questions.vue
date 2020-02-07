1c49166af312fe651a67b9a5f3ee797315499a82
cxh <564668501@qq.com>
2020-02-07 14:51:05 +0800

    基础题库-绘制搜索剩余表单域

--- src/module-hmmm/pages/questions.vue
+++ src/module-hmmm/pages/questions.vue
@@ -12,41 +12,115 @@
                 :key="item.value"
                 :label="item.label"
                 :value="item.value"
-              ></el-option> 
+              ></el-option>
             </el-select>
           </el-col>
           <el-col :span="6">
-            难度
+            难&nbsp;&nbsp;&nbsp;&nbsp;度
             <el-select v-model="searchForm.difficulty" placeholder="请选择" class="wh">
-               <el-option
+              <el-option
                 v-for="item in difficultyList"
                 :key="item.value"
                 :label="item.label"
                 :value="item.value"
-              ></el-option> 
+              ></el-option>
             </el-select>
           </el-col>
           <el-col :span="6">
             试题类型
             <el-select v-model="searchForm.questionType" placeholder="请选择" class="wh">
-             <el-option
+              <el-option
                 v-for="item in questionTypeList"
                 :key="item.value"
                 :label="item.label"
                 :value="item.value"
-              ></el-option> 
+              ></el-option>
             </el-select>
           </el-col>
           <el-col :span="6">
-            标签
-            <!-- <el-select v-model="value" placeholder="请选择" class="wh"> -->
+            标&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;签
+            <el-select v-model="searchForm.tags" placeholder="请选择" class="wh">
               <!-- <el-option
                 v-for="item in options"
                 :key="item.value"
                 :label="item.label"
                 :value="item.value"
+              ></el-option>-->
+            </el-select>
+          </el-col>
+        </el-row>
+        <!-- 第二行 -->
+        <el-row :gutter="20">
+          <el-col :span="6">
+            城市
+            <el-select v-model="searchForm.province" placeholder="选城市" style="width:109px">
+              <!-- <el-option
+                v-for="item in subjectIDList"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>-->
+            </el-select>
+            <el-select v-model="searchForm.city" placeholder="选区县" style="width:109px">
+              <!-- <el-option
+                v-for="item in subjectIDList"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>-->
+            </el-select>
+          </el-col>
+          <el-col :span="6">
+            关键字
+            <el-input v-model="searchForm.keyword" placeholder="请输入题目编号/题干" class="wh"></el-input>
+          </el-col>
+          <el-col :span="6">
+            题目备注
+            <el-input v-model="searchForm.remarks" placeholder="请输入" class="wh"></el-input>
+          </el-col>
+          <el-col :span="6">
+            企业简称
+            <el-input v-model="searchForm.shortName" placeholder="请输入" class="wh"></el-input>
+          </el-col>
+        </el-row>
+        <!-- 第三行 -->
+        <el-row :gutter="20">
+          <el-col :span="6">
+            方向
+            <el-select v-model="searchForm.direction" placeholder="请选择" class="wh">
+              <!-- <el-option
+                v-for="item in subjectIDList"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>-->
+            </el-select>
+          </el-col>
+          <el-col :span="6">
+            录入人
+            <el-select v-model="searchForm.creatorID" placeholder="请选择" class="wh">
+              <!-- <el-option
+                v-for="item in difficultyList"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>-->
+            </el-select>
+          </el-col>
+          <el-col :span="6">
+            二级目录
+            <el-select v-model="searchForm.catalogID" placeholder="请选择" class="wh">
+              <!-- <el-option
+                v-for="item in questionTypeList"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
               ></el-option> -->
-            <!-- </el-select> -->
+            </el-select>
+          </el-col>
+          <el-col :span="6">
+            <el-button size="mini">清除</el-button>
+            <el-button size="mini" type="primary">搜索</el-button>
           </el-col>
         </el-row>
       </el-card>
@@ -85,19 +159,23 @@ export default {
     this.getSubjectIDList()
     // console.log(this.difficultyList)  // 查看 难度
     // console.log(this.questionTypeList) // 查看 题型
-    
   },
   methods: {
     // 学科列表
-   async getSubjectIDList() {
-     let res = await simple()
-    //  console.log(res)
-     this.subjectIDList = res.data
-   }
+    async getSubjectIDList() {
+      let res = await simple()
+      //  console.log(res)
+      this.subjectIDList = res.data
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.el-input{
+  width: 223px;
+}
+.el-row{
+  margin-bottom: 10px;
+}
 </style>
